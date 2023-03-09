59b021b1de5503ecd2153837d84519127efeab85
Andy McKay <andy@clearwind.ca>
2023-03-09 15:12:21 -0800

    init commit

---
+++ content/about.md
@@ -0,0 +1,13 @@
+Title: About Build the Trail
+Date: 2022-03-09
+Summary: About this site
+
+This is site is about the Spirit Trail, a trail for pedestrians, cyclists and running across the North Shore. It was intended to run from Horseshoe Bay to Deep Cove through West Vancouver, the City of North Vancouver and the District of North Vancouver.
+
+In 2007 the plans to build the Spirit Trail were announced.
+
+In 2018 the City of North Vancouver [celebrated completing its part of the Spirit Trail](https://www.cnv.org/-/media/City-of-North-Vancouver/Documents/News-Releases/2018/2018-07-20-City-of-North-Vancouver-Celebrates-Completion-of-North-Shore-Spirit-Trail.pdf).
+
+In March 2023, 16 years after the original plans were created, the draft budget from the District of North Vancouver proposed deferring remaining work on the Spirit Trail for 5 years.
+
+This site is from advocates and concerned citizens who'd like to see this trail completed and provide a safe way for everyone to enjoy the North Shore away from cars.
---
+++ pelicanconf.py
@@ -0,0 +1,45 @@
+AUTHOR = 'Andy McKay'
+SITENAME = 'Build the Trail'
+SITEURL = 'http://localhost:8000'
+
+
+PATH = 'content'
+THEME = 'themes/Papyrus'
+THEME_STATIC_PATHS = ['static']
+PLUGIN_PATHS = ['pelican-plugins']
+PLUGINS = ['readtime', 'search', 'neighbors']
+
+TIMEZONE = 'America/Vancouver'
+
+DEFAULT_LANG = 'en'
+
+DISPLAY_PAGES_ON_MENU = True
+DIRECT_TEMPLATES = (('index', 'search', 'tags', 'categories', 'archives',))
+PAGINATED_TEMPLATES = {'index':None,'tag':None,'category':None,'author':None,'archives':24,}
+
+SEARCH_MODE = "output"
+SEARCH_HTML_SELECTOR = "main"
+TOC = {}
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = ()
+
+SHARE = (
+    ("twitter", "https://twitter.com/intent/tweet/?text=Features&amp;url="),
+    ("linkedin", "https://www.linkedin.com/sharing/share-offsite/?url="),
+    ("reddit", "https://reddit.com/submit?url="),
+    ("facebook", "https://facebook.com/sharer/sharer.php?u="),
+    ("whatsapp", "https://api.whatsapp.com/send?text=Features - "),
+    ("telegram", "https://telegram.me/share/url?text=Features&amp;url="),
+)
+
+DEFAULT_PAGINATION = False
+FAVICON_URLS = (
+    ('icon', 'images/icon.svg'),
+)
+
+SHOW_BREADCRUMBS = False
+SHOW_SUBSCRIBE = False
